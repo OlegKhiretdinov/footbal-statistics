faad96d5064c81a38d0447203c82e9bc462729be
OlegKhiretdinov <oleg@oleg.win>
2022-01-17 21:53:27 +0300

    fix: search override URL params

--- src/components/SearchInput/SearchInput.jsx
+++ src/components/SearchInput/SearchInput.jsx
@@ -1,11 +1,19 @@
 import { useSearchParams } from "react-router-dom"
 import { SEARCH } from "../../utils/const"
+import { urlStringToObject } from "../../utils/parseUrlParams"
 
 const SearchInput = () => {
   const [searchParams, setSearchParams] = useSearchParams()
 
   const onChange = (e) => {
-    setSearchParams({[SEARCH]: e.target.value})
+    const value = e.target.value
+    const paramsObj = urlStringToObject(searchParams.toString())
+    if (value) {
+      setSearchParams({...paramsObj, [SEARCH]: value})
+    } else {
+      delete paramsObj[SEARCH]
+      setSearchParams(paramsObj)
+    }
   }
 
   return (
